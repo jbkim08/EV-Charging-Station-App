6e7016cdda6ce95c6e314d87ea56e94bf3a90aae
JB <drv98@naver.com>
2024-01-08 15:06:52 +0900

    프리텐디드 폰트 적용

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
+import { useCallback } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Pretend-Regular': require('./assets/fonts/Pretendard-Regular.otf'),
+    'Pretend-Medium': require('./assets/fonts/Pretendard-Medium.otf'),
+    'Pretend-Bold': require('./assets/fonts/Pretendard-Bold.otf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={{ fontSize: 30 }}>프리텐디드 폰트</Text>
+      <Text style={{ fontFamily: 'Pretend-Regular', fontSize: 30 }}>세상에 이런 폰트가!</Text>
     </View>
   );
 }
