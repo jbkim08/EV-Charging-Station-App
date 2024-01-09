11dcecf3c4522073a4d413ccbd564082d7274671
JB <drv98@naver.com>
2024-01-09 09:42:10 +0900

    로그인스크린에 로고및 설명 추가

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
 import { useCallback } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
+import LoginScreen from './App/Screen/LoginScreen';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -24,8 +26,8 @@ export default function App() {
 
   return (
     <View style={styles.container} onLayout={onLayoutRootView}>
-      <Text style={{ fontSize: 30 }}>프리텐디드 폰트</Text>
-      <Text style={{ fontFamily: 'Pretend-Regular', fontSize: 30 }}>세상에 이런 폰트가!</Text>
+      <LoginScreen />
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -34,7 +36,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 25,
   },
 });
---
+++ App/Screen/LoginScreen.jsx
@@ -0,0 +1,55 @@
+import { View, Text, Image, StyleSheet } from 'react-native';
+import React from 'react';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+export default function LoginScreen() {
+  return (
+    <View
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 50,
+      }}
+    >
+      <Image source={require('./../../assets/images/logo.png')} style={styles.logoImage} />
+      <Image source={require('./../../assets/images/bg-logo.png')} style={styles.bgImage} />
+      <View style={{ padding: 20 }}>
+        <Text style={styles.heading}>EV 충전소를 찾는 APP</Text>
+        <Text style={styles.desc}>여러분 근처의 충전소를 빠르게 찾을 수 있습니다.</Text>
+        <View>
+          <Text>Login With Google</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  logoImage: {
+    width: '70%',
+    height: 60,
+    objectFit: 'contain',
+  },
+  bgImage: {
+    width: '100%',
+    height: 200,
+    marginTop: 20,
+    objectFit: 'cover',
+  },
+  heading: {
+    fontSize: 25,
+    fontFamily: 'Pretend-Bold',
+    textAlign: 'center',
+    marginTop: 20,
+  },
+  desc: {
+    fontSize: 17,
+    fontFamily: 'Pretend-Medium',
+    marginTop: 15,
+    textAlign: 'center',
+    color: '#000',
+    color: Colors,
+  },
+  button: {},
+});
