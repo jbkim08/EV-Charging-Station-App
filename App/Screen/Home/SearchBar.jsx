63f0631523e451459dad021688c850c0d52db379
JB <drv98@naver.com>
2024-01-15 12:30:48 +0900

    구글맵API적용

---
+++ App/Screen/Home/SearchBar.jsx
@@ -0,0 +1,23 @@
+import { View, Text } from 'react-native';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import React from 'react';
+
+export default function SearchBar({ searchedLocation }) {
+  return (
+    <View>
+      <GooglePlacesAutocomplete
+        placeholder="EV 충전소 찾기"
+        fetchDetails={true}
+        onPress={(data, details = null) => {
+          console.log('호출');
+          //console.log(data, details);
+          searchedLocation(details?.geometry?.location);
+        }}
+        query={{
+          key: 'AIzaSyBCec0VEJMZ2wdmqV2oSIIA291iwsoZ_k0',
+          language: 'en',
+        }}
+      />
+    </View>
+  );
+}
